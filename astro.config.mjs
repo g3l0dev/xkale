22e758da4bb5fc69aabdd1cd049990ac696fa932
Angelo94a <angelo94.aa@gmail.com>
2024-08-19 17:18:35 -0500

    [ADD] Agreganso sitiemap 🚀

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,16 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-
 import partytown from "@astrojs/partytown";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.xkale.com",
-
-  integrations: [
-    icon(),
-    image(),
-    tailwind(),
-    mdx(),
-    react(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [icon(), image(), tailwind(), mdx(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap()]
 });
